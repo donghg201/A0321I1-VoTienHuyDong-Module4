7148fdd7cdfae5d50673b568bb56e7340b05e138
donghg201@gmail.com <donghg201@gmail.com>
2021-11-19 19:22:21 +0700

    upload bai 4"

--- Bai4_views_and_thymeleaf/bai_tap/Manage_product/src/main/java/com/codegym/controller/ProductController.java
+++ Bai4_views_and_thymeleaf/bai_tap/Manage_product/src/main/java/com/codegym/controller/ProductController.java
@@ -1,33 +1,30 @@
 package com.codegym.controller;
 
 import com.codegym.model.Product;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+@Controller
 public class ProductController {
-    private static Map<Integer, Product> productMap = new HashMap<>();
+    private static List<Product> productList = new ArrayList<>();
 
     static {
-        productMap.put(1, new Product("A", "IP5", 10, "None"));
-        productMap.put(2, new Product("B", "IP6", 10, "None"));
-        productMap.put(3, new Product("C", "IP7", 10, "None"));
-        productMap.put(4, new Product("D", "IP8", 10, "None"));
-        productMap.put(5, new Product("E", "IPX", 10, "None"));
+        productList.add(new Product("A", "IP5", 10, "None"));
+        productList.add(new Product("B", "IP6", 10, "None"));
+        productList.add(new Product("C", "IP7", 10, "None"));
+        productList.add(new Product("D", "IP8", 10, "None"));
+        productList.add(new Product("E", "IPX", 10, "None"));
     }
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public String form(Model model) {
-        model.addAttribute("product", productMap);
+        model.addAttribute("productList", productList);
         return "index";
-    }
-
-    @PostMapping("/result")
-    public String settingResult(Model model, @ModelAttribute Product product) {
-        model.addAttribute("product", product);
-        return "result";
     }
 }
